6e06ab60d20d80c5b48c6d9bda2fb4fbeca4278f
김형택 <39832802+hyungtaik@users.noreply.github.com>
2020-03-08 01:28:42 +0900

    2020-03-07 [SWEA] 1251번 하나로  solve

---
+++ AlgorithmStudy/src/SSAFY/Solution_D4_1251_하나로_김형택_Kruskal.java
@@ -0,0 +1,107 @@
+package SSAFY;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Solution_D4_1251_하나로_김형택_Kruskal {
+
+	private static int TC;
+	private static int N;
+	private static Pair[] map;
+	private static double E;
+	private static int[] parents;
+	private static double[][] visit;
+	private static double result;
+	private static ArrayList<Pair> list;
+
+	public static void main(String[] args) throws Exception{	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		TC = Integer.parseInt(st.nextToken());
+		for(int tc=1;tc<=TC;tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			map = new Pair[N];
+			st = new StringTokenizer(br.readLine());
+			for(int i=0;i<N;i++) {
+				int x = Integer.parseInt(st.nextToken());
+				map[i] = new Pair(x,0,0);
+			}
+			st = new StringTokenizer(br.readLine());
+			for(int i=0;i<N;i++) {
+				int y = Integer.parseInt(st.nextToken());
+				map[i].y = y; 
+			}
+			st = new StringTokenizer(br.readLine());
+			E = Double.parseDouble(st.nextToken());
+
+			list = new ArrayList<Pair>();
+			
+			for(int i=0;i<N;i++) {
+				Pair p = map[i];
+				for(int j=i+1;j<N;j++) {
+					Pair t = map[j];
+					long num = (long) (Math.pow(p.x - t.x,2)+Math.pow(p.y - t.y,2));
+					list.add(new Pair(i,j,num));
+//					list.add(new Pair(j,i,num));
+				}
+			}
+			Collections.sort(list);
+			parents = new int[N];
+			makeSet();
+			
+			result = 0.0;
+			solve();
+			result*=E;
+			System.out.println("#"+tc+" "+Math.round(result));
+		}
+	}
+	
+	static void solve() {
+       for(int i=0;i<list.size();i++) {
+    	   Pair temp = list.get(i);
+    	   if(union(temp.x, temp.y)) {
+    		   result+=temp.num;
+    	   }
+       }
+	}
+	static void makeSet() {
+		Arrays.fill(parents, -1);
+	}
+	static int findSet(int x) {
+		if(parents[x]<0) return x;
+		return parents[x] = findSet(parents[x]); 
+	}
+	
+	static boolean union(int x,int y) {
+		int aRoot = findSet(x);
+		int bRoot = findSet(y);
+		
+		if(aRoot != bRoot) {
+			parents[bRoot] = aRoot;
+			return true;
+		}
+		return false;
+	}
+	
+	static class Pair implements Comparable<Pair>{
+		int x,y;
+		long num;
+		@Override
+		public int compareTo(Pair o) {
+			return Long.compare(this.num, o.num);
+		}
+		public Pair(int x, int y, long num) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.num = num;
+		}
+	}
+
+}
---
+++ AlgorithmStudy/src/SSAFY/Solution_D4_1251_하나로_김형택_Prim.java
@@ -0,0 +1,95 @@
+package SSAFY;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution_D4_1251_하나로_김형택_Prim {
+
+	private static int TC;
+	private static int N;
+	private static Pair[] map;
+	private static double E;
+	private static boolean[] visited;
+	private static double[][] visit;
+	private static double result;
+
+	public static void main(String[] args) throws Exception{	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		TC = Integer.parseInt(st.nextToken());
+		for(int tc=1;tc<=TC;tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			map = new Pair[N];
+			st = new StringTokenizer(br.readLine());
+			for(int i=0;i<N;i++) {
+				long x = Long.parseLong(st.nextToken());
+				map[i] = new Pair(x,0);
+			}
+			st = new StringTokenizer(br.readLine());
+			for(int i=0;i<N;i++) {
+				long y = Long.parseLong(st.nextToken());
+				map[i].y = y; 
+			}
+			st = new StringTokenizer(br.readLine());
+			E = Double.parseDouble(st.nextToken());
+			visited = new boolean[1000];
+			visit = new double[N][N];
+			
+			for(int i=0;i<N;i++) {
+				Pair p = map[i];
+				for(int j=i+1;j<N;j++) {
+					Pair t = map[j];
+					visit[i][j] = visit[j][i] = E*(Math.pow(p.x - t.x,2)+Math.pow(p.y - t.y,2));
+				}
+			}
+			result = 0.0;
+			solve();
+			System.out.println("#"+tc+" "+Math.round(result));
+		}
+	}
+	
+	static void solve() {
+        ArrayList<Integer> selected = new ArrayList<Integer>(N);
+        int index;
+        double min;
+        selected.add(0);
+        visited[0] = true;
+        for(int i = 0; i < N-1; i++) {
+            min = Double.MAX_VALUE;
+            index = -1;
+            for(Integer v : selected) {
+                for(int j = 0 ; j  < N; j++) { 
+                    if(!visited[j] && visit[v][j] != 0 && visit[v][j] < min) {
+                        min = visit[v][j];
+                        index = j;
+                    }
+                }
+            }
+             
+            if(index != -1) {
+                result += min;         
+                selected.add(index);    
+                visited[index] = true; 
+            }
+        }
+         
+        Arrays.fill(visited, false);
+	}
+	
+	
+	static class Pair{
+		long x,y;
+
+		public Pair(long x, long y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+}
