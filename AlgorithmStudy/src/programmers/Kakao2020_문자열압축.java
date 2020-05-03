b5773906a6e16ce9c437368bf49e80e5cc6962b7
김형택 <39832802+hyungtaik@users.noreply.github.com>
2020-05-03 19:01:25 +0900

    2020-05-04 [Programmers] Kakao2020_문자열압축.java solve

---
+++ AlgorithmStudy/src/programmers/Kakao2020_문자열압축.java
@@ -0,0 +1,73 @@
+package programmers;
+
+import java.util.LinkedList;
+
+public class Kakao2020_문자열압축 {
+
+	public static void main(String[] args) {
+		Solution temp = new Solution();
+		temp.solution("ababcdcdababcdcd");
+	}
+	static class Solution {
+		private LinkedList<Pair> q;
+		private int min;
+		public Solution() {
+		}
+	    public int solution(String s) {
+	        int answer = 0;
+	        int len = s.length();
+	        int pivot = 1;
+	        q = new LinkedList<Pair>();
+	        min = Integer.MAX_VALUE;
+	        while(pivot<=len/2) {
+	        	String temp = s.substring(0, pivot);
+	        	int count = 1;
+	        	for(int i=pivot;i<len;i+=pivot) {
+	        		
+	        		if(i+pivot > len) continue;
+	        		if(temp.equals(s.substring(i,i+pivot))) {
+	        			count++;
+	        		}else {
+	        			q.add(new Pair(temp,count));
+	        			temp = s.substring(i,i+pivot);
+	        			count=1;
+	        		}
+	        		if(i+pivot+pivot > len) {
+	        			q.add(new Pair(temp,count));
+	        			break;
+	        		}
+	        	}
+	        	int ll = len%pivot;
+	        	if(ll!=0) {
+	        		q.add(new Pair(s.substring(len-ll,len),1));
+	        	}
+	        	String result = "";
+	        	while(!q.isEmpty()) {
+	        		Pair tmp = q.poll();
+	        		if(tmp.count>1) {
+	        			result+=tmp.count+tmp.input;
+	        		}else {
+	        			result+=tmp.input;
+	        		}
+	        	}
+	        	min = Math.min(min,result.length());
+	        	pivot++;
+	        }
+	        if(min == Integer.MAX_VALUE) answer = len;
+	        else {
+	        	answer = min;
+	        }
+	        return answer;
+	    }
+	}
+	static class Pair{
+		String input;
+		int count;
+		public Pair(String input, int count) {
+			super();
+			this.input = input;
+			this.count = count;
+		}
+		
+	}
+}
